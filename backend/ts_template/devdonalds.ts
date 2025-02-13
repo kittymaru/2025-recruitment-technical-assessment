18bf11a868f9af42564801a3cc908a7e04982fe8
Cathy Yu <z5590011@ad.unsw.edu.au>
2025-02-13 22:48:08 +1100

    implemented and tested task 1

--- backend/ts_template/devdonalds.ts
+++ backend/ts_template/devdonalds.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
 
 // ==== Type Definitions, feel free to add or modify ==========================
 interface cookbookEntry {
@@ -45,8 +45,59 @@ app.post("/parse", (req:Request, res:Response) => {
 // [TASK 1] ====================================================================
 // Takes in a recipeName and returns it in a form that 
 const parse_handwriting = (recipeName: string): string | null => {
-  // TODO: implement me
-  return recipeName
+
+  // Check that input does not have length of > 0 characters
+  if (recipeName.length <= 0) {
+    return null;
+  }
+
+  // Remove leading and trailing whitespace
+  recipeName.trimEnd();
+  recipeName.trimStart();
+
+  // Initialise array for modified name
+  let newName: string = "";
+
+  // First iteration: remove all hyphens, underscores and invalid characters
+  for (let i: number = 0; i < recipeName.length; i++) {
+    if (recipeName[i] === "-" || recipeName[i] === "_" || recipeName[i] === " ") {
+      // Replace with space
+      newName = newName + " ";
+    } else if (/^[a-zA-Z]$/.test(recipeName[i]) == true) {
+      // Check if character is letter
+      newName = newName + recipeName[i];
+    }
+  }
+
+  if (newName.length <= 0) {
+    return null;
+  }
+
+  // Second iteration: remove extra spaces
+  let newNameSecond: string = "";
+  let isWord: boolean = false;
+
+  for (let j: number = 0; j < newName.length; j++) {
+    if (newName[j] !== " ") {
+      if (isWord === false) {
+        // First letter of word
+        newNameSecond = newNameSecond + newName[j].toUpperCase();
+        isWord = true;
+      } else {
+        // Remaining letters of word, convert to lowercase
+        newNameSecond = newNameSecond + newName[j].toLowerCase();
+      }
+    } else if (isWord === true) {
+      // Space reached, add one space only
+      newNameSecond = newNameSecond + " ";
+      isWord = false;
+    }
+  }
+
+  // Remove trailing whitespace
+  newNameSecond.trimEnd();
+
+  return newNameSecond;
 }
 
 // [TASK 2] ====================================================================
@@ -59,7 +110,7 @@ app.post("/entry", (req:Request, res:Response) => {
 
 // [TASK 3] ====================================================================
 // Endpoint that returns a summary of a recipe that corresponds to a query name
-app.get("/summary", (req:Request, res:Request) => {
+app.get("/summary", (req:Request, res:Response) => {
   // TODO: implement me
   res.status(500).send("not yet implemented!")
 
